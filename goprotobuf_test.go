5303ac198d1496a411bb029c7b314188bd0f92d6
Jacques Marais <jacquesmaraisrsa@gmail.com>
2018-08-10 12:37:51 +0000

    added protobuf3 bnf syntax and generated parser. 4 LR-1 conflicts

---
+++ goprotobuf_test.go
@@ -0,0 +1,54 @@
+// Copyright 2018 GoProtobuf Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package goprotobuf
+
+import (
+	"testing"
+)
+
+func TestCheck(t *testing.T) {
+	err := ParseBytes([]byte(testSimple))
+	if err != nil {
+		t.Fatalf("Failed with: %v", err)
+	}
+}
+
+var testString = `
+syntax = "proto3";
+import public "other.proto";
+option java_package = "com.example.foo";
+enum EnumAllowingAlias {
+  option allow_alias = true;
+  UNKNOWN = 0;
+  STARTED = 1;
+  RUNNING = 2 [(custom_option) = "hello world"];
+}
+message outer {
+  option (my_option).a = true;
+  message inner {   // Level 2
+    int64 ival = 1;
+  }
+  repeated inner inner_message = 2;
+  EnumAllowingAlias enum_field =3;
+  map<int32, string> my_map = 4;
+}
+`
+
+var testSimple = `
+syntax = "proto3";
+
+import public "other.proto";
+
+`
